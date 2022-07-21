d99b497bf773d90e135890e09ce2466c5d7e80db
Vikram Patel <vikram@Vikrams-MacBook-Pro-2.local>
2022-07-20 22:54:24 -0700

    Leetcode practice \nDetails: Added more test cases

--- src/test/java/com/eduvik/linkedlist/Leet92_ReverseLinkedList2Test.java
+++ src/test/java/com/eduvik/linkedlist/Leet92_ReverseLinkedList2Test.java
@@ -23,7 +23,7 @@ public class Leet92_ReverseLinkedList2Test {
   @Test
   public void reverseBetween_1() {
     ListNode head = getLinkedList();
-    ListNode ansHead = getReversedLinkedList();
+    ListNode ansHead = getReversedLinkedList1();
     ListNode rHead = object.reverseBetween(head, 2, 4);
     while (rHead != null) {
       assertEquals(rHead.val, ansHead.val);
@@ -32,6 +32,30 @@ public class Leet92_ReverseLinkedList2Test {
     }
   }
 
+  @Test
+  public void reverseBetween_2() {
+    ListNode head = getLinkedList();
+    ListNode ansHead = getReversedLinkedList2();
+    ListNode rHead = object.reverseBetween(head, 1, 5);
+    while (rHead != null) {
+      assertEquals(rHead.val, ansHead.val);
+      rHead = rHead.next;
+      ansHead = ansHead.next;
+    }
+  }
+  
+  @Test
+  public void reverseBetween_3() {
+    ListNode head = getLinkedList();
+    ListNode ansHead = head;
+    ListNode rHead = object.reverseBetween(head, 3, 3);
+    while (rHead != null) {
+      assertEquals(rHead.val, ansHead.val);
+      rHead = rHead.next;
+      ansHead = ansHead.next;
+    }
+  }
+  
   private ListNode getLinkedList() {
     ListNode head = new ListNode(5);
     head = new ListNode(4, head);
@@ -41,12 +65,21 @@ public class Leet92_ReverseLinkedList2Test {
     return head;
   }
 
-  private ListNode getReversedLinkedList() {
+  private ListNode getReversedLinkedList1() {
     ListNode head = new ListNode(5);
     head = new ListNode(2, head);
     head = new ListNode(3, head);
     head = new ListNode(4, head);
     head = new ListNode(1, head);
+    return head;
+  }
+  
+  private ListNode getReversedLinkedList2() {
+    ListNode head = new ListNode(1);
+    head = new ListNode(2, head);
+    head = new ListNode(3, head);
+    head = new ListNode(4, head);
+    head = new ListNode(5, head);
     return head;
   }
 }
